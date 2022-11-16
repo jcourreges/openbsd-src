04028ec7f7d01ad914bd79d30c8dbd4d6ff195fb
Patrick Wildt <patrick@blueri.se>
2022-11-16 23:05:47 +0100

    llvm: adjust makefiles

---
+++ gnu/usr.bin/clang/clang/main.cpp
@@ -0,0 +1,9 @@
+/* Public domain */
+
+extern int clang_main(int Argc, char **Argv);
+
+int
+main (int Argc, char **Argv)
+{
+	return clang_main(Argc, Argv);
+}
---
+++ gnu/usr.bin/clang/llvm-ar/main.cpp
@@ -0,0 +1,9 @@
+/* Public domain */
+
+extern int llvm_ar_main(int Argc, char **Argv);
+
+int
+main (int Argc, char **Argv)
+{
+	return llvm_ar_main(Argc, Argv);
+}
