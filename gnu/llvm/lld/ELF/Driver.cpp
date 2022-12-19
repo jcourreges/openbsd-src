11ac39c578aa854fce08253d251234fdf0423c1d
Patrick Wildt <patrick@blueri.se>
2022-12-19 23:24:44 +0100

    llvm: revert to 13.0.0

--- gnu/llvm/lld/ELF/Driver.cpp
+++ gnu/llvm/lld/ELF/Driver.cpp
@@ -358,16 +358,8 @@ static void checkOptions() {
   }
 
   if (config->executeOnly) {
-    switch (config->emachine) {
-    case EM_AARCH64:
-    case EM_RISCV:
-    case EM_MIPS:
-    case EM_SPARCV9:
-    case EM_X86_64:
-      break;
-    default:
-      error("-execute-only is not supported on this target");
-    }
+    if (config->emachine != EM_AARCH64)
+      error("-execute-only is only supported on AArch64 targets");
 
     if (config->singleRoRx && !script->hasSectionsCommand)
       error("-execute-only and -no-rosegment cannot be used together");
@@ -463,7 +455,6 @@ static bool isKnownZFlag(StringRef s) {
          s == "nodefaultlib" || s == "nodelete" || s == "nodlopen" ||
          s == "noexecstack" || s == "nognustack" ||
          s == "nokeep-text-section-prefix" || s == "norelro" ||
-         s == "noretpolineplt" ||
          s == "noseparate-code" || s == "nostart-stop-gc" || s == "notext" ||
          s == "now" || s == "origin" || s == "pac-plt" || s == "rel" ||
          s == "rela" || s == "relro" || s == "retpolineplt" ||
@@ -1071,8 +1062,7 @@ static void readConfigs(opt::InputArgList &args) {
   config->ignoreDataAddressEquality =
       args.hasArg(OPT_ignore_data_address_equality);
   config->ignoreFunctionAddressEquality =
-      args.hasFlag(OPT_ignore_function_address_equality,
-      OPT_no_ignore_function_address_equality, true);
+      args.hasArg(OPT_ignore_function_address_equality);
   config->init = args.getLastArgValue(OPT_init, "_init");
   config->ltoAAPipeline = args.getLastArgValue(OPT_lto_aa_pipeline);
   config->ltoCSProfileGenerate = args.hasArg(OPT_lto_cs_profile_generate);
@@ -1126,12 +1116,7 @@ static void readConfigs(opt::InputArgList &args) {
   config->optimize = args::getInteger(args, OPT_O, 1);
   config->orphanHandling = getOrphanHandling(args);
   config->outputFile = args.getLastArgValue(OPT_o);
-#ifdef __OpenBSD__
-  config->pie = args.hasFlag(OPT_pie, OPT_no_pie,
-      !args.hasArg(OPT_shared) && !args.hasArg(OPT_relocatable));
-#else
   config->pie = args.hasFlag(OPT_pie, OPT_no_pie, false);
-#endif
   config->printIcfSections =
       args.hasFlag(OPT_print_icf_sections, OPT_no_print_icf_sections, false);
   config->printGcSections =
@@ -1201,11 +1186,7 @@ static void readConfigs(opt::InputArgList &args) {
   config->zOrigin = hasZOption(args, "origin");
   config->zPacPlt = hasZOption(args, "pac-plt");
   config->zRelro = getZFlag(args, "relro", "norelro", true);
-#ifndef __OpenBSD__
-  config->zRetpolineplt = getZFlag(args, "retpolineplt", "noretpolineplt", false);
-#else
-  config->zRetpolineplt = getZFlag(args, "retpolineplt", "noretpolineplt", true);
-#endif
+  config->zRetpolineplt = hasZOption(args, "retpolineplt");
   config->zRodynamic = hasZOption(args, "rodynamic");
   config->zSeparate = getZSeparate(args);
   config->zShstk = hasZOption(args, "shstk");
@@ -1626,7 +1607,7 @@ void LinkerDriver::inferMachineType() {
 }
 
 // Parse -z max-page-size=<value>. The default value is defined by
-// each target. Is set to 1 if given nmagic or omagic.
+// each target.
 static uint64_t getMaxPageSize(opt::InputArgList &args) {
   uint64_t val = args::getZOptionValue(args, OPT_z, "max-page-size",
                                        target->defaultMaxPageSize);
@@ -1641,7 +1622,7 @@ static uint64_t getMaxPageSize(opt::InputArgList &args) {
 }
 
 // Parse -z common-page-size=<value>. The default value is defined by
-// each target. Is set to 1 if given nmagic or omagic.
+// each target.
 static uint64_t getCommonPageSize(opt::InputArgList &args) {
   uint64_t val = args::getZOptionValue(args, OPT_z, "common-page-size",
                                        target->defaultCommonPageSize);
@@ -1655,16 +1636,6 @@ static uint64_t getCommonPageSize(opt::InputArgList &args) {
   // commonPageSize can't be larger than maxPageSize.
   if (val > config->maxPageSize)
     val = config->maxPageSize;
-  return val;
-}
-
-// Parse -z max-page-size=<value>. The default value is defined by
-// each target.
-static uint64_t getRealMaxPageSize(opt::InputArgList &args) {
-  uint64_t val = args::getZOptionValue(args, OPT_z, "max-page-size",
-                                       target->defaultMaxPageSize);
-  if (!isPowerOf2_64(val))
-    error("max-page-size: value isn't a power of 2");
   return val;
 }
 
@@ -2454,11 +2425,6 @@ template <class ELFT> void LinkerDriver::link(opt::InputArgList &args) {
   // optimizations such as DATA_SEGMENT_ALIGN in linker scripts. LLD's use of it
   // is limited to writing trap instructions on the last executable segment.
   config->commonPageSize = getCommonPageSize(args);
-  // textAlignPageSize is the alignment page size to use when aligning PT_LOAD
-  // sections. This is the same as maxPageSize except under -omagic, where data
-  // sections are non-aligned (maxPageSize set to 1) but text sections are aligned
-  // to the target page size.
-  config->textAlignPageSize = config->omagic ? getRealMaxPageSize(args) : config->maxPageSize;
 
   config->imageBase = getImageBase(args);
 
